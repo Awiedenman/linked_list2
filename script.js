972cfcbe7553ba5bf7363010066d293784279a9f
Austin.Wiedenman <awiedenman@gmail.com>
2018-02-04 14:21:31 -0700

    Add delete functionality to our cards

--- script.js
+++ script.js
@@ -4,43 +4,38 @@ var cardURL = document.querySelector('.main-left__input-URL');
 var enterBtn = document.querySelector('.main-left__button-enter');
 
 enterBtn.addEventListener('click', appendCard);
-
-function Card(title, url) {
- this.title = cardTitle.value;
- console.log(cardTitle.value)
- this.url = cardURL.value;
-}  
-
-function createNewCard(title, url){
-  var newCard = new Card (title, url)  
-}
+cardList.addEventListener('click', deleteCard);
 
 function appendCard() {
-var createCard = document.createElement('li');  
+  var createCard = document.createElement('li');  
 
-cardList.appendChild(createCard); 
-     createCard.innerHTML =`
-     <li>
-        <h3 
-          class="main-right__card-title">
-          ${cardTitle.value}
-        </h3>
-        <a 
-          class="main-right__card-link" 
-          href="">${cardURL.value}
-        </a>
-        <button 
-          class= "main-right__card-button main-right__card-button-read">
-          Read
-        </button>
-        <button 
-          class= "main-right__card-button main-right__card-button-delete">
-          Delete
-        </button>
-      </li>`;
+  createCard.innerHTML = `
+    <h3 
+      class="main-right__card-title">
+      ${cardTitle.value}
+    </h3>
+    <a 
+      class="main-right__card-link" 
+      href="">${cardURL.value}
+    </a>
+    <button 
+      class= "main-right__card-button main-right__card-button-read">
+      Read
+    </button>
+    <button 
+      class= "main-right__card-button main-right__card-button-delete">
+      Delete
+    </button>
+    `;
 
-  createNewCard(cardTitle.value, cardURL.value);
+  cardList.appendChild(createCard); 
 }
 
+
+function deleteCard(event) {
+  if(event.target.matches(".main-right__card-button-delete")){
+    event.target.closest('li').remove();
+  }
+}
 
 
