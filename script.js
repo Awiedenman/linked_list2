8c24103031579d87ff1b4c4579e79b0a0cfbe3e2
Austin.Wiedenman <awiedenman@gmail.com>
2018-02-07 09:35:14 -0700

    Add CSS, fix read/delete buttons, add counter

--- script.js
+++ script.js
@@ -2,62 +2,122 @@ var cardList = document.querySelector('.main-right__card-list');
 var cardTitle = document.querySelector('.main-left__input-title');
 var cardURL = document.querySelector('.main-left__input-URL');
 var enterBtn = document.querySelector('.main-left__button-enter');
+var onPageCount = 0;
+var onPageRead = 0;
+var onPageUnread = 0;
+
 
 // enterBtn.addEventListener('click', appendCard);
-enterBtn.addEventListener('click', blankOnEntry);
+cardList.addEventListener('keyup', disabledButton);
+cardURL.addEventListener('keyup', disabledButton);
+enterBtn.addEventListener('click', appendCard);
+enterBtn.addEventListener('click', numOnPageIncrement);
+enterBtn.addEventListener('click', numUnreadIncrement);
 cardList.addEventListener('click', deleteCard);
 cardList.addEventListener('click', markAsRead);
+
 
 
 function appendCard() {
   var createCard = document.createElement('li');  
 
   createCard.innerHTML = `
-    <h3 
-      class="main-right__card-title">
-      ${cardTitle.value}
-    </h3>
-    <a 
-      class="main-right__card-link" 
-      href="">${cardURL.value}
-    </a>
-    <button 
-      class= "main-right__card-button main-right__card-button-read">
-      Read
-    </button>
-    <button 
-      class= "main-right__card-button main-right__card-button-delete">
-      Delete
-    </button>
+    <div class= 'card__container-title'>
+      <h3 
+        class="main-right__card-title">
+        ${cardTitle.value}
+      </h3>
+    </div>
+    <div class= 'card__container-link'>
+      <a 
+        class="main-right__card-link" 
+        href="">${cardURL.value}
+      </a>
+    </div>
+    <div class= 'card__container-button'>
+      <button 
+        class= "main-right__card-button main-right__card-button-read">
+        Read
+      </button>
+      <button 
+        class= "main-right__card-button main-right__card-button-delete">
+        Delete
+      </button>
+    </div>
     `;
 
-  cardList.appendChild(createCard); 
+  cardList.appendChild(createCard);
+  clearInputs(); 
+  disableOnEnter()
 }
 
+function numOnPageIncrement() {
+  onPageCount++
+  var numOnPage = document.querySelector('.main-left__counter-on-page')
+  numOnPage.innerText = onPageCount;
+}
+
+function numUnreadIncrement() {
+  onPageUnread++
+  var numUnread = document.querySelector('.main-left__counter-unread')
+  numUnread.innerText = onPageUnread;
+}
+
+function numReadIncrement() {
+  onPageRead++
+  var numOfRead = document.querySelector('.main-left__counter-read')
+  numOfRead.innerText = onPageRead;
+}
 
 function deleteCard(event) {
-  if(event.target.matches('.main-right__card-button-delete')){
+  if(event.target && event.target.matches('.main-right__card-button-delete')){
     event.target.closest('li').remove();
   }
 }
 
-function markAsRead() {
+function markAsRead(event) {
   if (event.target && event.target.matches('.main-right__card-button-read')){
-    event.target.closest('li').classList.toggle('read');
+    var card = event.target.closest('li');
+    console.log(card)
+    card.classList.toggle('read');
+
+  event.target.classList.toggle('main-right__card-button-read-clicked');
+
+  numReadIncrement();
   }
-  document.querySelector('.main-right__card-button-read').classList.toggle('main-right__card-button-read-clicked');
-  document.querySelector('.main-right__card-button-delete').classList.toggle('main-right__card-button-delete-read');
 }
  
+// ------------------------------
+  // var sibling = document.querySelector('.main-right__card-button-read').nextSibling
+  // var trueSibling = document.querySelector('.main-right__card-button-read').nextSibling
+   // var deleteBtn = document.querySelector('.main-right__card-button-delete');
+   // console.log(deleteBtn)
+   // deleteBtn.classList.toggle('main-right__card-button-delete-read');
+
 // function unmarkAsRead() {
 //   document.querySelector('.main-right__card-button-read').style.color = '#445A64'
 // }
+// --------------------------------
 
-function blankOnEntry() {
-  if (cardURL.value === '' || cardTitle.value === ''){
-    alert('Error empty field')
+function clearInputs() {
+  cardURL.value = '';
+  cardTitle.value = '';
+  console.log(clearInputs);
+}
+
+function disableOnEnter() {
+  enterBtn.setAttribute('disabled', true)
+}
+
+function disabledButton() {
+  if (cardURL.value === '' && cardTitle.value === '') {
+    alert('Error, empty field')
+    enterBtn.setAttribute('disabled', true)
+  } else if (cardURL.value === '' || cardTitle.value === '') {
+    alert('Error, empty field')
+    enterBtn.setAttribute('disabled', true)
   } else {
-    appendCard()
+    enterBtn.disabled = false;
   }
 }
 
